d5cb6272a1e393502b506364db3c4d9b27c823ac
Li Yin <liyin@scripps.edu>
2018-10-03 15:09:04 -0700

    Replaced f-string with .format()

--- parser.py
+++ parser.py
@@ -6,13 +6,13 @@ from csv import DictReader
 from biothings.utils.dataload import open_anyfile
 
 DATA_SCHEMA = ('chrom', 'start', 'end', 'ncer_percentile')
-SOURCE_NAME = 'mvcgi'
+SOURCE_NAME = 'julia'
 
 
 def load_data(data_folder):
 
     input_file = os.path.join(data_folder, 'julia_data')
-    assert os.path.exists(input_file), f'Cannot find input file {input_file}'
+    assert os.path.exists(input_file), 'Cannot find input file {}'.format(input_file)
     with open_anyfile(input_file) as in_f:
         # field names
         fields = DATA_SCHEMA
@@ -23,7 +23,7 @@ def load_data(data_folder):
         results = defaultdict(list)
         for row in reader:
             # construct identifier (e.g. chr1:g.678900_679000)
-            _id = f'{row["chrom"]}:g.{row["start"]}_{row["end"]}'
+            _id = '{chrom}:g.{start}_{end}'.format(chrom=row["chrom"], start=row["start"], end=row["end"])
             variant = {k: unicodedata.normalize("NFKD", v) for k, v in row.items()}
             # variant = dict_sweep(variant, vals=['', 'null', 'N/A', None, [], {}])
             results[_id].append(variant)
